3f4838d68610c24cee79b4a520b8f5044c5a068f
Ygor Mendes <ygortmendes@gmail.com>
2024-09-29 15:57:15 -0300

    fix: change preview img and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,16 @@ import "./page.scss";
 export const metadata: Metadata = {
   title: "segurobp",
   description: "Seguros automotivos.",
+  openGraph: {
+    images: [
+      {
+        url: "https://segurobp.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FwhiteLogo.1c8c124a.png&w=828&q=75",
+        width: 1200,
+        height: 630,
+        alt: "Imagem de preview do SeguroBP",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
